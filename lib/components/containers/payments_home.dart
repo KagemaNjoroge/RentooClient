0e292a43df2f04b933ce76e3451bf00ebd4f4b35
Kagema Njoroge <reecejames934@gmail.com>
2024-07-19 23:06:09 +0300

    feat: Add payment settings and payment modal to PaymentsHome

--- lib/components/containers/payments_home.dart
+++ lib/components/containers/payments_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rentoo_pms/utils/leases.dart';
 
 import '../common/tenant_selector.dart';
 
@@ -9,7 +10,123 @@ class PaymentsHome extends StatefulWidget {
   State<PaymentsHome> createState() => _PaymentsHomeState();
 }
 
+var lease = 0;
+
 class _PaymentsHomeState extends State<PaymentsHome> {
+  Widget addPaymentModal() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            "Add payment",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          // lease selector
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              FutureBuilder(
+                future: fetchLeases(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    if (snapshot.data!['status'] == "success") {
+                      var leases = snapshot.data!['leases'];
+                      List<DropdownMenuItem> leaseItems = [];
+                      for (var lease in leases) {
+                        leaseItems.add(DropdownMenuItem(
+                          value: lease.id,
+                          child: Text(lease.id.toString()),
+                        ));
+                      }
+                      return DropdownButtonFormField(
+                        icon: const Icon(Icons.arrow_downward),
+                        items: leaseItems,
+                        onChanged: (value) {
+                          setState(() {
+                            lease = value as int;
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          labelText: "Lease",
+                          hintText: "Select lease",
+                        ),
+                      );
+                    }
+                  }
+
+                  return const CircularProgressIndicator();
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: "Amount",
+                  hintText: "Enter amount",
+                ),
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  label: const Text("Cancel"),
+                  icon: const Icon(Icons.close),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  label: const Text("Add"),
+                  icon: const Icon(Icons.done),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget paymentSettingsModal() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          const Text(
+            "Payment settings",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Payment settings form goes here"),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Close"),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,7 +145,13 @@ class _PaymentsHomeState extends State<PaymentsHome> {
                   ElevatedButton.icon(
                     icon: const Icon(Icons.settings),
                     label: const Text("Payment settings"),
-                    onPressed: () {},
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return paymentSettingsModal();
+                          });
+                    },
                   ),
                   const SizedBox(
                     width: 10,
@@ -36,7 +159,13 @@ class _PaymentsHomeState extends State<PaymentsHome> {
                   ElevatedButton.icon(
                     icon: const Icon(Icons.add),
                     label: const Text("Add payment"),
-                    onPressed: () {},
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return addPaymentModal();
+                          });
+                    },
                   )
                 ],
               )
