8e89bb9d936b5d8d5ed35580a4d781dfa60dfdec
Kagema Njoroge <reecejames934@gmail.com>
2024-07-19 23:06:32 +0300

    refactor: Initialize BrightnessProvider with saved theme from SharedPreferences

--- lib/providers/brightness.dart
+++ lib/providers/brightness.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BrightnessProvider with ChangeNotifier {
   bool isDark = false;
 
+  BrightnessProvider() {
+    init();
+  }
+
+  void init() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDark = prefs.getBool('isDark') ?? false;
+    notifyListeners();
+  }
+
+  void saveTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDark', isDark);
+  }
+
   void swithTheme() {
     isDark = !isDark;
+    saveTheme();
     notifyListeners();
   }
 }
