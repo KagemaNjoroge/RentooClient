ff73b7f523a224f2d2fca3b60aaa15423b4348ed
Kagema Njoroge <reecejames934@gmail.com>
2024-07-20 17:32:12 +0300

    refactor: Add UnitsAPI class for handling unit-related API requests

---
+++ lib/sdk/units.dart
@@ -0,0 +1,141 @@
+import 'package:dio/dio.dart';
+
+import '../models/unit.dart';
+import 'base.dart';
+
+class UnitsAPI implements BaseApi {
+  final Dio _dio = Dio();
+
+  @override
+  Future<Map<String, dynamic>> get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var units = <Unit>[];
+        for (var prop in response.data) {
+          units.add(Unit.fromJson(prop));
+        }
+
+        return {
+          "status": "success",
+          "units": units,
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load units",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.post(url, data: body);
+      if (response.statusCode == 201) {
+        return {
+          "status": "success",
+          "unit": Unit.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to create unit",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.put(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "unit": Unit.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to update unit",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> delete(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.delete(url, queryParameters: queryParameters);
+      if (response.statusCode == 204) {
+        return {
+          "status": "success",
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to delete unit",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> patch(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "unit": Unit.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to patch unit",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  dynamic handleError(dynamic e) {
+    if (e is DioException) {
+      String? message = e.message;
+      if (e.response != null) {
+        message = e.response?.data.toString();
+      }
+      return {
+        "status": "error",
+        "message": message,
+      };
+    } else {
+      return {
+        "status": "error",
+        "message": e.toString(),
+      };
+    }
+  }
+}
