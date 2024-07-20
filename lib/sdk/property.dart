ea064c75f6ab4f06198244091d4685d84f8b28ad
Kagema Njoroge <reecejames934@gmail.com>
2024-07-20 12:56:23 +0300

    refactor: Update project metadata revision to b0850beeb25f6d5b10426284f506557f66181b36

---
+++ lib/sdk/property.dart
@@ -0,0 +1,278 @@
+import 'package:dio/dio.dart';
+
+import '../models/house.dart';
+import '../models/property.dart';
+import 'base.dart';
+
+class PropertyAPI implements BaseApi {
+  final Dio _dio = Dio();
+  @override
+  Future<Map<String, dynamic>> get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var property = <Property>[];
+        for (var prop in response.data) {
+          property.add(Property.fromJson(prop));
+        }
+
+        return {
+          "status": "success",
+          "property": property,
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.post(url, data: body);
+      if (response.statusCode == 201) {
+        return {
+          "status": "success",
+          "property": Property.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to create property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.put(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "property": Property.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to update property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> delete(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.delete(url, queryParameters: queryParameters);
+      if (response.statusCode == 204) {
+        return {
+          "status": "success",
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to delete property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> patch(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "property": Property.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to patch property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  dynamic handleError(dynamic e) {
+    if (e is DioException) {
+      String? message = e.message;
+      if (e.response != null) {
+        message = e.response?.data.toString();
+      }
+      return {
+        "status": "error",
+        "message": message,
+      };
+    } else {
+      return {
+        "status": "error",
+        "message": e.toString(),
+      };
+    }
+  }
+}
+
+class HousesAPI implements BaseApi {
+  final Dio _dio = Dio();
+
+  @override
+  Future<Map<String, dynamic>> get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var houses = <House>[];
+        for (var prop in response.data) {
+          houses.add(House.fromJson(prop));
+        }
+
+        return {
+          "status": "success",
+          "houses": houses,
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load houses",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.post(url, data: body);
+      if (response.statusCode == 201) {
+        return {
+          "status": "success",
+          "house": House.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to create house",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.put(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "house": House.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to update property",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> delete(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.delete(url, queryParameters: queryParameters);
+      if (response.statusCode == 204) {
+        return {
+          "status": "success",
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to delete house",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> patch(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "house": House.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to patch house",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  dynamic handleError(dynamic e) {
+    if (e is DioException) {
+      String? message = e.message;
+      if (e.response != null) {
+        message = e.response?.data.toString();
+      }
+      return {
+        "status": "error",
+        "message": message,
+      };
+    } else {
+      return {
+        "status": "error",
+        "message": e.toString(),
+      };
+    }
+  }
+}
