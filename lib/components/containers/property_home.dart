aef5e1b9e8f5857aa0d7bd9278b50dd26df86d81
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 01:45:16 +0300

    feat: Add HorizontalGap and Gap components for creating horizontal and vertical spacing

--- lib/components/containers/property_home.dart
+++ lib/components/containers/property_home.dart
@@ -1,7 +1,180 @@
 import 'package:flutter/material.dart';
-import 'package:rentoo_pms/sdk/property.dart';
 
 import '../../constants.dart';
+import '../../models/property.dart';
+import '../../sdk/property.dart';
+import '../../utils/snack.dart';
+import '../common/gap.dart';
+
+class AddPropertyBottomSheet extends StatefulWidget {
+  const AddPropertyBottomSheet({super.key});
+
+  @override
+  State<AddPropertyBottomSheet> createState() => _AddPropertyBottomSheetState();
+}
+
+class _AddPropertyBottomSheetState extends State<AddPropertyBottomSheet> {
+  final PropertyAPI _propertyAPI = PropertyAPI();
+  // controller for the add property modal
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _addressController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _purposeController = TextEditingController();
+  // keys
+  final _formKey = GlobalKey<FormState>();
+
+  // state
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          left: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          right: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Name is required";
+                }
+                return null;
+              },
+              controller: _nameController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.abc_outlined),
+                labelText: "Property Name",
+              ),
+            ),
+            const Gap(),
+            DropdownButtonFormField(
+              validator: (value) {
+                if (value == null) {
+                  return "Purpose is required";
+                }
+                return null;
+              },
+              items: purposes
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e["value"],
+                      child: Text(e["name"]),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) {
+                _purposeController.text = value.toString();
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.category),
+                labelText: "Purpose",
+              ),
+            ),
+            const Gap(),
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Address is required";
+                }
+                return null;
+              },
+              controller: _addressController,
+              minLines: 2,
+              maxLines: 3,
+              decoration: const InputDecoration(
+                labelText: "Where is it located?",
+                icon: Icon(Icons.location_on),
+              ),
+            ),
+            const Gap(),
+            TextFormField(
+              controller: _descriptionController,
+              minLines: 3,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                labelText: "Some few details about the property",
+                icon: Icon(Icons.description),
+              ),
+            ),
+            const Gap(),
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  label: const Text("Close"),
+                ),
+                TextButton.icon(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      Property property = Property(
+                        name: _nameController.text,
+                        address: _addressController.text,
+                        description: _descriptionController.text,
+                        purpose: _purposeController.text,
+                        photos: [],
+                      );
+                      try {
+                        setState(() {
+                          _isLoading = true;
+                        });
+
+                        var response = await _propertyAPI.post(propertyUrl,
+                            body: property.toJson());
+                        if (response['status'] == 'success') {
+                          showSnackBar(context, Colors.green,
+                              "Property added successfully", 300);
+                          Navigator.pop(context);
+                        } else {
+                          showSnackBar(
+                              context, Colors.red, "An error occurred", 300);
+                        }
+                      } catch (e) {
+                        showSnackBar(context, Colors.red, e.toString(), 600);
+                      } finally {
+                        setState(() {
+                          _isLoading = false;
+                        });
+                      }
+                    }
+                  },
+                  icon: const Icon(Icons.done),
+                  label: _isLoading
+                      ? const CircularProgressIndicator.adaptive()
+                      : const Text("Save"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class PropertyHome extends StatefulWidget {
   const PropertyHome({super.key});
@@ -12,120 +185,8 @@ class PropertyHome extends StatefulWidget {
 
 class _PropertyHomeState extends State<PropertyHome> {
   final PropertyAPI _propertyAPI = PropertyAPI();
-  // controller for the add property modal
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _addressController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  final TextEditingController _purposeController = TextEditingController();
-  // keys
-  final _formKey = GlobalKey<FormState>();
-  final Widget _gap = const SizedBox(height: 20);
-  final bool _propertyExists = true;
 
-  Widget addPropertyModal() {
-    // name, address, description
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return "Name is required";
-              }
-              return null;
-            },
-            controller: _nameController,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.abc_outlined),
-              labelText: "Property Name",
-            ),
-          ),
-          _gap,
-          DropdownButtonFormField(
-            validator: (value) {
-              if (value == null) {
-                return "Purpose is required";
-              }
-              return null;
-            },
-            items: purposes
-                .map(
-                  (e) => DropdownMenuItem(
-                    value: e["value"],
-                    child: Text(e["name"]),
-                  ),
-                )
-                .toList(),
-            onChanged: (value) {
-              _purposeController.text = value.toString();
-            },
-            decoration: const InputDecoration(
-              icon: Icon(Icons.category),
-              labelText: "Purpose",
-            ),
-          ),
-          _gap,
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return "Address is required";
-              }
-              return null;
-            },
-            controller: _addressController,
-            minLines: 2,
-            maxLines: 3,
-            decoration: const InputDecoration(
-              labelText: "Where is it located?",
-              icon: Icon(Icons.location_on),
-            ),
-          ),
-          _gap,
-          TextFormField(
-            controller: _descriptionController,
-            minLines: 3,
-            maxLines: 5,
-            decoration: const InputDecoration(
-              labelText: "Some few details about the property",
-              icon: Icon(Icons.description),
-            ),
-          ),
-          _gap,
-          Row(
-            children: [
-              TextButton.icon(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.close),
-                label: const Text("Close"),
-              ),
-              TextButton.icon(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    Navigator.pop(context);
-                    // show snackbar
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Property added"),
-                        behavior: SnackBarBehavior.floating,
-                        backgroundColor: Colors.green,
-                        width: 200,
-                      ),
-                    );
-                  }
-                },
-                icon: const Icon(Icons.done),
-                label: const Text("Save"),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
+  final bool _propertyExists = true;
 
   List<int> selectedProperties = [];
   bool allSelected = false;
@@ -152,33 +213,11 @@ class _PropertyHomeState extends State<PropertyHome> {
                   ElevatedButton.icon(
                     onPressed: () {
                       showBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return Container(
-                              padding: const EdgeInsets.all(20),
-                              decoration: const BoxDecoration(
-                                border: Border(
-                                  top: BorderSide(
-                                    color: Colors.grey,
-                                    width: 1,
-                                  ),
-                                  left: BorderSide(
-                                    color: Colors.grey,
-                                    width: 1,
-                                  ),
-                                  right: BorderSide(
-                                    color: Colors.grey,
-                                    width: 1,
-                                  ),
-                                ),
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(20),
-                                  topRight: Radius.circular(20),
-                                ),
-                              ),
-                              child: addPropertyModal(),
-                            );
-                          });
+                        context: context,
+                        builder: (context) {
+                          return const AddPropertyBottomSheet();
+                        },
+                      );
                     },
                     label: const Text("Add Property"),
                     icon: const Icon(Icons.add),
@@ -210,82 +249,92 @@ class _PropertyHomeState extends State<PropertyHome> {
             padding: const EdgeInsets.all(8),
             width: double.infinity,
             child: SingleChildScrollView(
-                child: FutureBuilder(
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Row(
-                    children: [
-                      Icon(Icons.dangerous),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Text("An error occurred")
-                    ],
-                  );
-                }
-                if (snapshot.connectionState == ConnectionState.done &&
-                    snapshot.hasData) {
-                  var props = snapshot.data!['property'];
-
-                  return DataTable(
-                    columns: const [
-                      DataColumn(label: Text("Name")),
-                      DataColumn(label: Text("Address")),
-                    ],
-                    rows: [
-                      for (var property in props)
-                        DataRow(
-                          cells: [
-                            DataCell(TextButton(
-                              child: Text(property.name ?? ''),
-                              onPressed: () {
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: const Text("Attach action here"),
-                                    behavior: SnackBarBehavior.floating,
-                                    backgroundColor: Colors.green,
-                                    width: 220,
-                                    action: SnackBarAction(
-                                      label: "Close",
-                                      onPressed: () =>
-                                          ScaffoldMessenger.of(context)
-                                              .clearSnackBars(),
-                                    ),
-                                  ),
-                                );
-                              },
-                            )),
-                            DataCell(Text(property.address ?? '')),
-                          ],
+              child: FutureBuilder(
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Row(
+                      children: [
+                        Icon(Icons.dangerous),
+                        SizedBox(
+                          width: 5,
                         ),
-                    ],
-                  );
-                }
-                if (snapshot.connectionState == ConnectionState.done &&
-                    !snapshot.hasData) {
-                  return Row(
-                    children: [
-                      const Icon(Icons.dangerous),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      const Text("No property found"),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      ElevatedButton.icon(
-                        label: const Text("Add property"),
-                        icon: const Icon(Icons.add),
-                        onPressed: () {},
-                      )
-                    ],
-                  );
-                }
-                return const Center(
-                    child: CircularProgressIndicator.adaptive());
-              },
-              future: _propertyAPI.get(propertyUrl),
-            )),
+                        Text("An error occurred")
+                      ],
+                    );
+                  }
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.hasData) {
+                    var props = snapshot.data!['property'];
+
+                    return DataTable(
+                      columns: const [
+                        DataColumn(
+                          label: Text("Name"),
+                        ),
+                        DataColumn(
+                          label: Text("Address"),
+                        ),
+                      ],
+                      rows: [
+                        for (var property in props)
+                          DataRow(
+                            cells: [
+                              DataCell(
+                                TextButton(
+                                  child: Text(property.name ?? ''),
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        content:
+                                            const Text("Attach action here"),
+                                        behavior: SnackBarBehavior.floating,
+                                        backgroundColor: Colors.green,
+                                        width: 220,
+                                        action: SnackBarAction(
+                                          label: "Close",
+                                          onPressed: () =>
+                                              ScaffoldMessenger.of(context)
+                                                  .clearSnackBars(),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                              DataCell(
+                                Text(property.address ?? ''),
+                              ),
+                            ],
+                          ),
+                      ],
+                    );
+                  }
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      !snapshot.hasData) {
+                    return Row(
+                      children: [
+                        const Icon(Icons.dangerous),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        const Text("No property found"),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        ElevatedButton.icon(
+                          label: const Text("Add property"),
+                          icon: const Icon(Icons.add),
+                          onPressed: () {},
+                        )
+                      ],
+                    );
+                  }
+                  return const Center(
+                      child: CircularProgressIndicator.adaptive());
+                },
+                future: _propertyAPI.get(propertyUrl),
+              ),
+            ),
           ),
         ),
       ],
