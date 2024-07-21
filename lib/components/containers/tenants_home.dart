3b2755250aa32eb056f1192302e5af03ab6ba767
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 01:14:21 +0300

    feat: Add HorizontalGap and Gap components for creating horizontal and vertical spacing

--- lib/components/containers/tenants_home.dart
+++ lib/components/containers/tenants_home.dart
@@ -1,9 +1,219 @@
 import 'package:flutter/material.dart';
-import 'package:rentoo_pms/utils/snack.dart';
 
 import '../../constants.dart';
 import '../../models/tenant.dart';
 import '../../sdk/tenants.dart';
+import '../../utils/snack.dart';
+import '../common/gap.dart';
+
+class AddTenantBottomSheet extends StatefulWidget {
+  const AddTenantBottomSheet({super.key});
+
+  @override
+  State<AddTenantBottomSheet> createState() => _AddTenantBottomSheetState();
+}
+
+class _AddTenantBottomSheetState extends State<AddTenantBottomSheet> {
+  final TenantsAPI _tenantsAPI = TenantsAPI();
+  bool _isLoading = false;
+
+  List<DropdownMenuItem> _tenantsTypes() {
+    return tenantTypes
+        .map(
+          (e) => DropdownMenuItem(
+            value: e['value'],
+            child: Text(e['name']!),
+          ),
+        )
+        .toList();
+  }
+
+  String tenantType = "Individual";
+
+// controllers
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _websiteController = TextEditingController();
+// keys
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          left: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          right: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            DropdownButtonFormField(
+              items: _tenantsTypes(),
+              onChanged: (value) {
+                tenantType = value.toString();
+              },
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                hintText: "Tenant Type",
+              ),
+            ),
+            const Gap(),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.name,
+                    controller: _firstNameController,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "First Name is required";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        border: UnderlineInputBorder(),
+                        hintText: "First Name"),
+                  ),
+                ),
+                const HorizontalGap(),
+                Expanded(
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Last Name is required";
+                      }
+                      return null;
+                    },
+                    controller: _lastNameController,
+                    keyboardType: TextInputType.name,
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.person),
+                        border: UnderlineInputBorder(),
+                        hintText: "Last Name"),
+                  ),
+                ),
+              ],
+            ),
+            const Gap(),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _phoneController,
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.phone),
+                        border: UnderlineInputBorder(),
+                        hintText: "Phone Number"),
+                  ),
+                ),
+                const HorizontalGap(),
+                Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        border: UnderlineInputBorder(),
+                        hintText: "Email"),
+                  ),
+                ),
+              ],
+            ),
+            const Gap(),
+            TextFormField(
+              controller: _websiteController,
+              keyboardType: TextInputType.url,
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.language),
+                  border: UnderlineInputBorder(),
+                  hintText: "Website"),
+            ),
+            const Gap(),
+            Row(
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  label: const Text("Close"),
+                  icon: const Icon(Icons.close),
+                ),
+                TextButton.icon(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      Tenant tenant = Tenant(
+                        firstName: _firstNameController.text,
+                        lastName: _lastNameController.text,
+                        email: _emailController.text,
+                        phoneNumber: _phoneController.text,
+                        type: tenantType,
+                        website: _websiteController.text,
+                        houses: [],
+                      );
+                      // save
+                      _isLoading = true;
+                      try {
+                        var response = await _tenantsAPI.post(tenantsUrl,
+                            body: tenant.toJson());
+                        if (response['status'] == "success") {
+                          showSnackBar(context, Colors.green,
+                              "Tenant added successfully", 300);
+                          // clear fields
+                          _firstNameController.clear();
+                          _lastNameController.clear();
+                          _emailController.clear();
+                          _phoneController.clear();
+                          _websiteController.clear();
+                        } else {
+                          showSnackBar(
+                              context, Colors.red, "An error occurred", 300);
+                        }
+                        _isLoading = false;
+                      } catch (e) {
+                        showSnackBar(context, Colors.red, e.toString(), 300);
+
+                        _isLoading = false;
+                      } finally {
+                        setState(() {});
+                      }
+                    }
+                  },
+                  label: _isLoading
+                      ? const CircularProgressIndicator.adaptive()
+                      : const Text("Save"),
+                  icon: const Icon(Icons.done),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class TenantsHome extends StatefulWidget {
   const TenantsHome({super.key});
@@ -14,13 +224,13 @@ class TenantsHome extends StatefulWidget {
 
 class _TenantsHomeState extends State<TenantsHome> {
   bool _isLoading = false;
-  Widget _gap() {
+  Widget Gap() {
     return const SizedBox(
       height: 10,
     );
   }
 
-  Widget _horizontalGap() {
+  Widget HorizontalGap() {
     return const SizedBox(
       width: 10,
     );
@@ -37,7 +247,7 @@ class _TenantsHomeState extends State<TenantsHome> {
         .toList();
   }
 
-  String tenant_type = "Individual";
+  String tenantType = "Individual";
 
 // controllers
   final TextEditingController _firstNameController = TextEditingController();
@@ -79,16 +289,14 @@ class _TenantsHomeState extends State<TenantsHome> {
             DropdownButtonFormField(
               items: _tenantsTypes(),
               onChanged: (value) {
-                setState(() {
-                  tenant_type = value.toString();
-                });
+                tenantType = value.toString();
               },
               decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
                 hintText: "Tenant Type",
               ),
             ),
-            _gap(),
+            Gap(),
             Row(
               children: [
                 Expanded(
@@ -107,7 +315,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                         hintText: "First Name"),
                   ),
                 ),
-                _horizontalGap(),
+                HorizontalGap(),
                 Expanded(
                   child: TextFormField(
                     validator: (value) {
@@ -126,7 +334,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                 ),
               ],
             ),
-            _gap(),
+            Gap(),
             Row(
               children: [
                 Expanded(
@@ -139,7 +347,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                         hintText: "Phone Number"),
                   ),
                 ),
-                _horizontalGap(),
+                HorizontalGap(),
                 Expanded(
                   child: TextFormField(
                     keyboardType: TextInputType.emailAddress,
@@ -152,7 +360,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                 ),
               ],
             ),
-            _gap(),
+            Gap(),
             TextFormField(
               controller: _websiteController,
               keyboardType: TextInputType.url,
@@ -161,7 +369,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                   border: UnderlineInputBorder(),
                   hintText: "Website"),
             ),
-            _gap(),
+            Gap(),
             Row(
               children: [
                 TextButton.icon(
@@ -179,7 +387,7 @@ class _TenantsHomeState extends State<TenantsHome> {
                         lastName: _lastNameController.text,
                         email: _emailController.text,
                         phoneNumber: _phoneController.text,
-                        type: tenant_type,
+                        type: tenantType,
                         website: _websiteController.text,
                         houses: [],
                       );
@@ -188,8 +396,11 @@ class _TenantsHomeState extends State<TenantsHome> {
                         _isLoading = true;
                       });
                       try {
-                        var response = await _tenantsAPI.post(tenantsUrl,
-                            body: tenant.toJson());
+                        var response = await _tenantsAPI.post(
+                          tenantsUrl,
+                          body: tenant.toJson(),
+                        );
+
                         if (response['status'] == "success") {
                           showSnackBar(context, Colors.green,
                               "Tenant added successfully", 300);
@@ -199,15 +410,15 @@ class _TenantsHomeState extends State<TenantsHome> {
                           _emailController.clear();
                           _phoneController.clear();
                           _websiteController.clear();
+
+                          Navigator.pop(context);
                         } else {
                           showSnackBar(
                               context, Colors.red, "An error occurred", 300);
                         }
-                        setState(() {
-                          _isLoading = false;
-                        });
                       } catch (e) {
                         showSnackBar(context, Colors.red, e.toString(), 300);
+                      } finally {
                         setState(() {
                           _isLoading = false;
                         });
@@ -246,15 +457,16 @@ class _TenantsHomeState extends State<TenantsHome> {
                 ),
               ),
               ElevatedButton.icon(
-                  onPressed: () {
-                    showBottomSheet(
-                        context: context,
-                        builder: (context) {
-                          return _addTenantModal();
-                        });
-                  },
-                  label: const Text("Add Tenant"),
-                  icon: const Icon(Icons.add)),
+                onPressed: () {
+                  showBottomSheet(
+                      context: context,
+                      builder: (context) {
+                        return _addTenantModal();
+                      });
+                },
+                label: const Text("Add Tenant"),
+                icon: const Icon(Icons.add),
+              ),
             ],
           ),
         ),
@@ -285,21 +497,8 @@ class _TenantsHomeState extends State<TenantsHome> {
                     var tenant = snapshot.data!['tenants'][index];
                     return ListTile(
                       onTap: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content:
-                                Text("${tenant.firstName} ${tenant.lastName}"),
-                            behavior: SnackBarBehavior.floating,
-                            width: 300,
-                            backgroundColor: Colors.green,
-                            action: SnackBarAction(
-                              label: "Close",
-                              onPressed: () {
-                                ScaffoldMessenger.of(context).clearSnackBars();
-                              },
-                            ),
-                          ),
-                        );
+                        showSnackBar(context, Colors.green,
+                            "${tenant.firstName} ${tenant.lastName}", 300);
                       },
                       leading: CircleAvatar(
                         backgroundImage: tenant.photo != null
