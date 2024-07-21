1f49cae20cbc8a846bbf89166b11d799d1833d29
Kagema Njoroge <reecejames934@gmail.com>
2024-07-21 19:28:10 +0300

    refactor: Update URLs for company, properties, houses, tenants, leases, notifications, units, and agents

--- lib/components/containers/houses_home.dart
+++ lib/components/containers/houses_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rentoo_pms/components/common/gap.dart';
 
 import '../../constants.dart';
 import '../../sdk/property.dart';
@@ -23,83 +24,136 @@ class _HousesHomeState extends State<HousesHome> {
 
   final Widget _gap = const SizedBox(height: 20);
   Widget addHouseModal() {
-    return Form(
-      key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: const Column(
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          left: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+          right: BorderSide(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+      ),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: const Column(
+                children: [
+                  Text("At which property is the house located in?*"),
+                  PropertySelector(),
+                ],
+              ),
+            ),
+            _gap,
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "House number is required";
+                }
+                return null;
+              },
+              controller: _houseNumberController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.home),
+                labelText: "House Number*",
+              ),
+            ),
+            _gap,
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Rent is required";
+                }
+                return null;
+              },
+              controller: _rentController,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.money),
+                labelText: "Rent*",
+              ),
+            ),
+            _gap,
+            DropdownButtonFormField(
+              validator: (value) {
+                if (value == null) {
+                  return "Purpose is required";
+                }
+                return null;
+              },
+              items: purposes
+                  .map(
+                    (e) => DropdownMenuItem(
+                      value: e["value"],
+                      child: Text(e["name"]),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) {
+                _purposeController.text = value.toString();
+              },
+              decoration: const InputDecoration(
+                icon: Icon(Icons.category),
+                labelText: "Purpose",
+              ),
+            ),
+            _gap,
+            TextFormField(
+              controller: _descriptionController,
+              minLines: 3,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                icon: Icon(Icons.description),
+                labelText: "Description",
+              ),
+            ),
+            const Gap(),
+            Row(
               children: [
-                Text("At which property is the house located in?*"),
-                PropertySelector(),
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(Icons.close),
+                  label: const Text("Cancel"),
+                ),
+                TextButton.icon(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // save house
+                      Navigator.pop(context);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("House saved"),
+                          behavior: SnackBarBehavior.floating,
+                          width: 200,
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                    }
+                  },
+                  icon: const Icon(Icons.done),
+                  label: const Text("Save"),
+                ),
               ],
-            ),
-          ),
-          _gap,
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return "House number is required";
-              }
-              return null;
-            },
-            controller: _houseNumberController,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.home),
-              labelText: "House Number*",
-            ),
-          ),
-          _gap,
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return "Rent is required";
-              }
-              return null;
-            },
-            controller: _rentController,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.money),
-              labelText: "Rent*",
-            ),
-          ),
-          _gap,
-          DropdownButtonFormField(
-            validator: (value) {
-              if (value == null) {
-                return "Purpose is required";
-              }
-              return null;
-            },
-            items: purposes
-                .map(
-                  (e) => DropdownMenuItem(
-                    value: e["value"],
-                    child: Text(e["name"]),
-                  ),
-                )
-                .toList(),
-            onChanged: (value) {
-              _purposeController.text = value.toString();
-            },
-            decoration: const InputDecoration(
-              icon: Icon(Icons.category),
-              labelText: "Purpose",
-            ),
-          ),
-          _gap,
-          TextFormField(
-            controller: _descriptionController,
-            minLines: 3,
-            maxLines: 5,
-            decoration: const InputDecoration(
-              icon: Icon(Icons.description),
-              labelText: "Description",
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -123,41 +177,9 @@ class _HousesHomeState extends State<HousesHome> {
               ),
               ElevatedButton.icon(
                 onPressed: () {
-                  showDialog(
+                  showBottomSheet(
                     context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text("Add House"),
-                        content: addHouseModal(),
-                        actions: [
-                          TextButton.icon(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(Icons.close),
-                            label: const Text("Cancel"),
-                          ),
-                          TextButton.icon(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                // save house
-                                Navigator.pop(context);
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    content: Text("House saved"),
-                                    behavior: SnackBarBehavior.floating,
-                                    width: 200,
-                                    backgroundColor: Colors.green,
-                                  ),
-                                );
-                              }
-                            },
-                            icon: const Icon(Icons.done),
-                            label: const Text("Save"),
-                          ),
-                        ],
-                      );
-                    },
+                    builder: (context) => addHouseModal(),
                   );
                 },
                 icon: const Icon(Icons.add),
@@ -190,12 +212,24 @@ class _HousesHomeState extends State<HousesHome> {
                               mainAxisSize: MainAxisSize.min,
                               children: [
                                 Icon(Icons.warning, color: Colors.red),
-                                Text("No houses found"),
+                                SizedBox(width: 10),
+                                Text(
+                                  "No houses found",
+                                  style: TextStyle(color: Colors.red),
+                                  textAlign: TextAlign.center,
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                ),
                               ],
                             ),
                             const SizedBox(height: 20),
                             ElevatedButton.icon(
-                              onPressed: () {},
+                              onPressed: () {
+                                showBottomSheet(
+                                  context: context,
+                                  builder: (context) => addHouseModal(),
+                                );
+                              },
                               icon: const Icon(Icons.add),
                               label: const Text("Add House"),
                             ),
