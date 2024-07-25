eae48c15d2a16f00cbc62d600095cbadd2f91220
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:13:55 +0300

    refactor: Add file_selector dependency for file-related operations

--- lib/sdk/units.dart
+++ lib/sdk/units.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:file_selector/file_selector.dart';
 
 import '../models/unit.dart';
 import 'base.dart';
@@ -137,5 +138,11 @@ class UnitsAPI implements BaseApi {
         "message": e.toString(),
       };
     }
+  }
+
+  @override
+  Future<Map<String, dynamic>> uploadFile(
+      String url, XFile file, String field) {
+    throw UnimplementedError();
   }
 }
