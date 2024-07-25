3db46610b229d99a2849c59f9c2233cd7eaccc0c
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:14:22 +0300

    feat: Add file upload functionality to BaseApi

--- lib/sdk/base.dart
+++ lib/sdk/base.dart
@@ -1,3 +1,5 @@
+import 'package:file_selector/file_selector.dart';
+
 abstract class BaseApi {
   Future<Map<String, dynamic>> get(String url,
       {Map<String, dynamic>? queryParameters});
@@ -7,4 +9,6 @@ abstract class BaseApi {
       {Map<String, dynamic>? queryParameters});
   Future<Map<String, dynamic>> patch(String url, {Map<String, dynamic>? body});
   dynamic handleError(dynamic e);
+
+  Future<Map<String, dynamic>> uploadFile(String url, XFile file, String field);
 }
