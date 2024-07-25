08847a1b2602b2a9b69ae9a544d2d476e5c6edce
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:14:49 +0300

    refactor: Remove unused properties and dependencies in User model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,5 @@
 class User {
   int? id;
-  DateTime? lastLogin;
   bool? isSuperUser;
   String? username;
   String? firstName;
@@ -8,20 +7,16 @@ class User {
   String? email;
   bool? isStaff;
   bool? isActive;
-  DateTime? dateJoined;
   String? phoneNumber;
   String? profilePicture;
   String? website;
   String? bio;
   String? location;
-  DateTime? birthDate = DateTime.now();
   String? idNumber;
   String? kraPin;
 
   User({
     this.bio,
-    this.birthDate,
-    this.dateJoined,
     this.email,
     this.firstName,
     this.id,
@@ -30,7 +25,6 @@ class User {
     this.isStaff,
     this.isSuperUser,
     this.kraPin,
-    this.lastLogin,
     this.lastName,
     this.location,
     this.phoneNumber,
@@ -42,7 +36,6 @@ class User {
   Map<String, dynamic> toJson() {
     return {
       "id": id,
-      "last_login": lastLogin,
       "is_superuser": isSuperUser,
       "username": username,
       "first_name": firstName,
@@ -50,13 +43,11 @@ class User {
       "email": email,
       "is_staff": isStaff,
       "is_active": isActive,
-      "date_joined": dateJoined,
       "phone_number": phoneNumber,
       "profile_picture": profilePicture,
       "website": website,
       "bio": bio,
       "location": location,
-      "birth_date": birthDate,
       "id_number": idNumber,
       "kra_pin": kraPin
     };
@@ -64,22 +55,19 @@ class User {
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    lastLogin = DateTime.parse(json['last_login']);
     isSuperUser = json['is_superuser'];
-    username = json['username'] ?? '';
-    firstName = json['first_name'] ?? '';
-    lastName = json['last_name'] ?? '';
-    email = json['email'] ?? '';
+    username = json['username'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
+    email = json['email'];
     isStaff = json['is_staff'];
     isActive = json['is_active'];
-    dateJoined = DateTime.parse(json['date_joined']);
-    phoneNumber = json['phone_number'] ?? '';
-    profilePicture = json['profile_picture'] ?? '';
-    website = json['website'] ?? '';
-    bio = json['bio'] ?? '';
-    location = json['location'] ?? '';
-    birthDate = DateTime.tryParse(json['birth_date']) ?? DateTime.now();
-    idNumber = json['id_number'] ?? '';
-    kraPin = json['kra_pin'] ?? '';
+    phoneNumber = json['phone_number'];
+    profilePicture = json['profile_picture'];
+    website = json['website'];
+    bio = json['bio'];
+    location = json['location'];
+    idNumber = json['id_number'];
+    kraPin = json['kra_pin'];
   }
 }
