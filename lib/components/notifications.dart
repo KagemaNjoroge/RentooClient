efddbbffc24a9159ff3c011bbc378c5d450ba1d5
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:13:02 +0300

    refactor: Improve notifications modal rendering logic

--- lib/components/notifications.dart
+++ lib/components/notifications.dart
@@ -31,6 +31,17 @@ class _NotificationsModalState extends State<NotificationsModal> {
           List notifications = snapshot.data!['notifications'];
           List<Widget> notifs = [];
 
+          if (notifications.isEmpty) {
+            return const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(
+                  child: Text("You are all caught up!"),
+                ),
+              ],
+            );
+          }
+
           for (var x in notifications) {
             notifs.add(ListTile(
               title: Text(
@@ -50,11 +61,6 @@ class _NotificationsModalState extends State<NotificationsModal> {
             mainAxisSize: MainAxisSize.min,
             children: notifs,
           );
-        } else if (snapshot.connectionState == ConnectionState.done &&
-            !snapshot.hasData) {
-          return const Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [Icon(Icons.dangerous), Text("No notifications")]);
         }
         return const Column(
           mainAxisSize: MainAxisSize.min,
