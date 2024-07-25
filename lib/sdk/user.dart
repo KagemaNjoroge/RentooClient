2d9ab1f17d326d83bb85a1bfbaeb4ae3f8dfdc30
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 10:16:35 +0300

    feat: Add UserAPI class for user-related API operations

---
+++ lib/sdk/user.dart
@@ -0,0 +1,140 @@
+import 'package:dio/dio.dart';
+
+import '../models/user.dart';
+import 'base.dart';
+
+class UserAPI implements BaseApi {
+  final Dio _dio = Dio();
+  @override
+  Future<Map<String, dynamic>> get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var users = <User>[];
+        print("Data: ${response.data}");
+        for (var prop in response.data) {
+          users.add(User.fromJson(prop));
+        }
+        return {
+          "status": "success",
+          "users": users,
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load users",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.post(url, data: body);
+      if (response.statusCode == 201) {
+        return {
+          "status": "success",
+          "user": User.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to create user",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.put(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "user": User.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to update user",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> delete(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.delete(url, queryParameters: queryParameters);
+      if (response.statusCode == 204) {
+        return {
+          "status": "success",
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to delete user",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> patch(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "user": User.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to patch user",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  dynamic handleError(dynamic e) {
+    if (e is DioException) {
+      String? message = e.message;
+      if (e.response != null) {
+        message = e.response?.data.toString();
+      }
+      return {
+        "status": "error",
+        "message": message,
+      };
+    } else {
+      return {
+        "status": "error",
+        "message": e.toString(),
+      };
+    }
+  }
+}
