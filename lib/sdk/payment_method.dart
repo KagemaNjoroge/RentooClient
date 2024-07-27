bb0ca612c6f30af61b691aec0594ce2b6ce91475
KagemaNjoroge <reecejames934@gmail.com>
2024-07-27 20:54:30 +0300

    feat: Create PaymentMethodAPI for interaction with the REST endpoint

---
+++ lib/sdk/payment_method.dart
@@ -0,0 +1,171 @@
+import 'package:dio/dio.dart';
+import 'package:file_selector/file_selector.dart';
+
+import '../models/payment_method.dart';
+import 'base.dart';
+
+class PaymentMethodMethodAPI implements BaseApi {
+  final Dio _dio = Dio();
+  @override
+  Future<Map<String, dynamic>> get(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var payments = <PaymentMethod>[];
+        for (var prop in response.data) {
+          payments.add(PaymentMethod.fromJson(prop));
+        }
+        return {
+          "status": "success",
+          "payment_methods": payments,
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load payment methods",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.post(url, data: body);
+      if (response.statusCode == 201) {
+        return {
+          "status": "success",
+          "payment_method": PaymentMethod.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to create payment method",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.put(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "payment_method": PaymentMethod.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to update payment method",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> delete(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.delete(url, queryParameters: queryParameters);
+      if (response.statusCode == 204) {
+        return {
+          "status": "success",
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to delete payment method",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> patch(String url,
+      {Map<String, dynamic>? body}) async {
+    try {
+      final response = await _dio.patch(url, data: body);
+      if (response.statusCode == 200) {
+        return {
+          "status": "success",
+          "payment_method": PaymentMethod.fromJson(response.data),
+        };
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to patch payment method",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+
+  @override
+  dynamic handleError(dynamic e) {
+    if (e is DioException) {
+      String? message = e.message;
+      if (e.response != null) {
+        message = e.response?.data.toString();
+      }
+      return {
+        "status": "error",
+        "message": message,
+      };
+    } else {
+      return {
+        "status": "error",
+        "message": e.toString(),
+      };
+    }
+  }
+
+  @override
+  Future<Map<String, dynamic>> uploadFile(
+      String url, XFile file, String field) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> getItem(String url,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(url, queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        var payment = PaymentMethod.fromJson(response.data);
+        return {
+          "status": "success",
+          "payment_method": payment,
+        };
+      } else if (response.statusCode == 404) {
+        return {"status": "error", "detail": "PaymentMethod not found"};
+      } else {
+        throw DioException(
+          requestOptions: response.requestOptions,
+          response: response,
+          error: "Failed to load payment method",
+        );
+      }
+    } catch (e) {
+      throw handleError(e);
+    }
+  }
+}
