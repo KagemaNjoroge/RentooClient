48efeb0a878b12fc2c7b7c211df6b8aada892760
KagemaNjoroge <reecejames934@gmail.com>
2024-07-27 22:42:48 +0300

    feat: Update Lease model with date parsing and default values

--- lib/models/lease.dart
+++ lib/models/lease.dart
@@ -10,38 +10,37 @@ class Lease {
   int? tenant;
   int? house;
 
-  Lease({
-    this.id,
-    this.startDate,
-    this.endDate,
-    this.depositAmount,
-    this.isActive,
-    this.createdAt,
-    this.updatedAt,
-    this.renewMonthly,
-    this.tenant,
-    this.house,
-  });
+  Lease(
+      {this.id,
+      this.startDate,
+      this.endDate,
+      this.depositAmount,
+      this.isActive,
+      this.createdAt,
+      this.updatedAt,
+      this.renewMonthly,
+      this.house,
+      this.tenant});
 
   Map<String, dynamic> toJson() {
     return {
       "id": id,
-      "start_date": startDate,
-      "end_date": endDate,
+      "start_date": startDate.toString(),
+      "end_date": endDate.toString(),
       "deposit_amount": depositAmount,
       "is_active": isActive,
-      "created_at": createdAt,
-      "updated_at": updatedAt,
+      "created_at": createdAt.toString(),
+      "updated_at": updatedAt.toString(),
       "renew_monthly": renewMonthly,
       "tenant": tenant,
-      "house": house,
+      "house": house
     };
   }
 
   Lease.fromJson(Map<String, dynamic> json) {
     id = json["id"];
-    startDate = DateTime.parse(json['start_date']);
-    endDate = DateTime.parse(json['end_date']);
+    startDate = DateTime.tryParse(json['start_date']) ?? DateTime.now();
+    endDate = DateTime.tryParse(json['end_date']) ?? DateTime.now();
     createdAt = DateTime.parse(json['created_at']);
     updatedAt = DateTime.parse(json['updated_at']);
     depositAmount = double.parse(json['deposit_amount']);
