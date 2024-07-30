3f330718a4ee6baea9ae800d7d3ef2946e5f4215
KagemaNjoroge <reecejames934@gmail.com>
2024-07-30 21:05:37 +0300

    Refactor leases_home.dart to use DataTable for displaying lease information

--- lib/components/containers/payments_home.dart
+++ lib/components/containers/payments_home.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:rentoo_pms/models/payment.dart';
-import 'package:rentoo_pms/sdk/payment.dart';
+import 'package:rentoo_pms/models/payment_method.dart';
+import 'package:rentoo_pms/sdk/payment_method.dart';
+import 'package:rentoo_pms/utils/snack.dart';
 
 import '../../constants.dart';
+import '../../models/lease.dart';
+import '../../models/payment.dart';
 import '../../sdk/leases.dart';
+import '../../sdk/payment.dart';
+import '../../utils/dates_parser.dart';
+import '../common/gap.dart';
 
 class PaymentsHome extends StatefulWidget {
   const PaymentsHome({super.key});
@@ -12,96 +18,32 @@ class PaymentsHome extends StatefulWidget {
   State<PaymentsHome> createState() => _PaymentsHomeState();
 }
 
-var lease = 0;
-
 class _PaymentsHomeState extends State<PaymentsHome> {
-  final LeasesAPI _leasesAPI = LeasesAPI();
   final PaymentAPI _paymentAPI = PaymentAPI();
-  Widget addPaymentModal() {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text(
-            "Add payment",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          // lease selector
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              FutureBuilder(
-                future: _leasesAPI.get(leasesUrl),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done &&
-                      snapshot.hasData) {
-                    if (snapshot.data!['status'] == "success") {
-                      var leases = snapshot.data!['leases'];
-                      List<DropdownMenuItem> leaseItems = [];
-                      for (var lease in leases) {
-                        leaseItems.add(DropdownMenuItem(
-                          value: lease.id,
-                          child: Text(lease.id.toString()),
-                        ));
-                      }
-                      return DropdownButtonFormField(
-                        icon: const Icon(Icons.arrow_downward),
-                        items: leaseItems,
-                        onChanged: (value) {
-                          setState(() {
-                            lease = value as int;
-                          });
-                        },
-                        decoration: const InputDecoration(
-                          labelText: "Lease",
-                          hintText: "Select lease",
-                        ),
-                      );
-                    }
-                  }
 
-                  return const CircularProgressIndicator();
-                },
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Amount",
-                  hintText: "Enter amount",
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  label: const Text("Cancel"),
-                  icon: const Icon(Icons.close),
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  label: const Text("Add"),
-                  icon: const Icon(Icons.done),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+  Widget _parsePaymentStatus(String status) {
+    var wid = const Icon(Icons.device_unknown);
+    switch (status) {
+      case "Complete":
+        wid = const Icon(
+          Icons.verified,
+          color: Colors.blue,
+        );
+      case "Pending":
+        wid = const Icon(Icons.pending);
+      case "Cancelled":
+        wid = const Icon(
+          Icons.cancel_outlined,
+          color: Colors.red,
+        );
+      case "In progress":
+        wid = const Icon(Icons.more_horiz);
+
+        break;
+      default:
+        wid = const Icon(Icons.device_unknown);
+    }
+    return wid;
   }
 
   @override
@@ -124,7 +66,7 @@ class _PaymentsHomeState extends State<PaymentsHome> {
                   showModalBottomSheet(
                       context: context,
                       builder: (context) {
-                        return addPaymentModal();
+                        return const AddPaymentBottomSheet();
                       });
                 },
               ),
@@ -144,15 +86,51 @@ class _PaymentsHomeState extends State<PaymentsHome> {
                     child: Text("No payments found"),
                   );
                 } else {
-                  List<Widget> paymentTiles = [];
-                  for (var u in payments) {
-                    paymentTiles.add(ListTile(
-                      title: Text("${u.id}"),
-                      subtitle: Text("${u.createdAt}"),
-                    ));
+                  List<DataRow> items = [];
+                  for (var payment in payments) {
+                    items.add(
+                      DataRow(cells: [
+                        DataCell(
+                          Text("${payment.id ?? '_'}"),
+                        ),
+                        DataCell(
+                          Text("${payment.lease ?? '_'}"),
+                        ),
+                        DataCell(
+                          Text(
+                            parseDate(payment.paymentDate ?? DateTime.now()),
+                          ),
+                        ),
+                        DataCell(
+                          Text("${payment.amount ?? '_'}"),
+                        ),
+                        DataCell(
+                            _parsePaymentStatus(payment.paymentStatus ?? ''))
+                      ]),
+                    );
                   }
-                  return Column(
-                    children: paymentTiles,
+                  List<DataColumn> cols = [
+                    const DataColumn(
+                      label: Text("ID"),
+                    ),
+                    const DataColumn(
+                      label: Text("Lease"),
+                    ),
+                    const DataColumn(
+                      label: Text("Paid on"),
+                    ),
+                    const DataColumn(
+                      label: Text("Amount"),
+                    ),
+                    const DataColumn(label: Text("Status"))
+                  ];
+                  return SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        DataTable(columns: cols, rows: items),
+                      ],
+                    ),
                   );
                 }
               }
@@ -163,6 +141,213 @@ class _PaymentsHomeState extends State<PaymentsHome> {
           ),
         )
       ],
+    );
+  }
+}
+
+class AddPaymentBottomSheet extends StatefulWidget {
+  const AddPaymentBottomSheet({super.key});
+
+  @override
+  State<AddPaymentBottomSheet> createState() => _AddPaymentBottomSheetState();
+}
+
+class _AddPaymentBottomSheetState extends State<AddPaymentBottomSheet> {
+  final LeasesAPI _leasesAPI = LeasesAPI();
+  final PaymentMethodAPI _paymentMethodAPI = PaymentMethodAPI();
+  final PaymentAPI _paymentAPI = PaymentAPI();
+  var lease = 0;
+  String _method = "";
+  String _paymentStatus = '';
+  // controllers & keys
+  final GlobalKey<FormState> _key = GlobalKey();
+  final TextEditingController _amountController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // payment status dropdown items
+    List<DropdownMenuItem> statusItems = [];
+    for (var i in paymentStatuses) {
+      statusItems.add(
+        DropdownMenuItem(
+          value: i['name'],
+          child: Text(i['name']),
+        ),
+      );
+    }
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Add payment",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            const Gap(),
+            // lease selector
+            Form(
+              key: _key,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  FutureBuilder(
+                    future: _leasesAPI.get(leasesUrl),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done &&
+                          snapshot.hasData) {
+                        if (snapshot.data!['status'] == "success") {
+                          List<Lease> leases = snapshot.data!['leases'];
+                          List<DropdownMenuItem> leaseItems = [];
+                          for (var lease in leases) {
+                            leaseItems.add(
+                              DropdownMenuItem(
+                                value: lease.id,
+                                child: Text("${lease.id}"),
+                              ),
+                            );
+                          }
+                          return DropdownButtonFormField(
+                            validator: (value) {
+                              if (value.toString().isEmpty) {
+                                return "You have to select a lease";
+                              }
+                              return null;
+                            },
+                            icon: const Icon(Icons.arrow_downward),
+                            items: leaseItems,
+                            onChanged: (value) {
+                              lease = value as int;
+                            },
+                            decoration: const InputDecoration(
+                              labelText: "Lease",
+                              hintText: "Select lease",
+                            ),
+                          );
+                        }
+                      }
+
+                      return const CircularProgressIndicator();
+                    },
+                  ),
+                  const Gap(),
+                  TextFormField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    validator: (val) {
+                      try {
+                        double.parse(val!);
+                      } catch (e) {
+                        return e.toString();
+                      }
+                      if (val.isEmpty) {
+                        return "Amount is required";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "Amount",
+                      hintText: "Enter amount",
+                    ),
+                  ),
+                  const Gap(),
+                  FutureBuilder(
+                      future: _paymentMethodAPI.get(paymentMethodsUrl),
+                      builder: (_, snapshot) {
+                        if (snapshot.hasError) {
+                          return Column(
+                            children: [
+                              const Text("An error occurred"),
+                              Text(snapshot.error.toString())
+                            ],
+                          );
+                        }
+                        if (snapshot.connectionState == ConnectionState.done &&
+                            snapshot.hasData &&
+                            !snapshot.hasError) {
+                          List<PaymentMethod> methods =
+                              snapshot.data!['payment_methods'];
+                          List<DropdownMenuItem> items = [];
+                          for (var method in methods) {
+                            items.add(
+                              DropdownMenuItem(
+                                value: method.name,
+                                child: Text(method.name ?? ''),
+                              ),
+                            );
+                          }
+                          return DropdownButtonFormField(
+                              decoration: const InputDecoration(
+                                  hintText: "Payment Method"),
+                              items: items,
+                              onChanged: (val) {
+                                _method = val as String;
+                              });
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator.adaptive(),
+                        );
+                      }),
+                  const Gap(),
+                  DropdownButtonFormField(
+                      decoration:
+                          const InputDecoration(hintText: "Payment Status"),
+                      items: statusItems,
+                      onChanged: (val) {
+                        _paymentStatus = val as String;
+                      })
+                ],
+              ),
+            ),
+            const Gap(),
+            Row(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  label: const Text("Cancel"),
+                  icon: const Icon(Icons.close),
+                ),
+                const HorizontalGap(),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    if (_key.currentState!.validate()) {
+                      Payment payment = Payment(
+                        paymentMethod: _method,
+                        lease: lease,
+                        paymentDate: DateTime.now(),
+                        amount: double.parse(_amountController.text),
+                        paymentStatus: _paymentStatus,
+                        createdAt: DateTime.now(),
+                        updatedAt: DateTime.now(),
+                        notes: '',
+                        referenceCode: '',
+                      );
+
+                      var response = await _paymentAPI.post(paymentsUrl,
+                          body: payment.toJson());
+
+                      if (response['status'] == 'success') {
+                        Navigator.pop(context);
+                        showSnackBar(context, Colors.green,
+                            "Payment added successfully", 300);
+                      } else {
+                        showSnackBar(context, Colors.red,
+                            "An error occurred. Please try again.", 400);
+                      }
+                    }
+                  },
+                  label: const Text("Add"),
+                  icon: const Icon(Icons.done),
+                )
+              ],
+            ),
+            const Gap()
+          ],
+        ),
+      ),
     );
   }
 }
