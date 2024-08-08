975b645c86e9a6cb27f64728bf961e0e1ea7e8a6
KagemaNjoroge <reecejames934@gmail.com>
2024-08-08 22:42:59 +0300

    Refactor date parsing logic for consistency and readability

--- lib/utils/dates_parser.dart
+++ lib/utils/dates_parser.dart
@@ -1,7 +1,15 @@
 String parseDate(DateTime time) {
   var year = time.year;
-  var month = time.month;
-  var date = time.day;
+  var month = time.month.toString();
+  var date = time.day.toString();
 
-  return "$year-$month-$date";
+  if (int.parse(month) < 10) {
+    month = "0$month";
+  }
+
+  if (int.parse(date) < 10) {
+    date = "0$date";
+  }
+
+  return "$year-$date-$month";
 }
